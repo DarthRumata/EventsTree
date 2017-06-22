fc3f17df342166a7940c86eced6aca7dc9f8b4cf
Stas Kirichok <kirichok.stas@gmail.com>
2017-06-22 22:19:13 +0300

    [Add] Added example

---
+++ EventNodeExample/EventNodeExample/External/MWSegmentedControl.swift
@@ -0,0 +1,97 @@
+//
+//  MWSegmentedControl.swift
+//  90 Day
+//
+//  Created by Micah Wilson on 8/19/15.
+//  Copyright (c) 2015 Micah Wilson. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol MWSegmentedControlDelegate {
+  ///Set when segment changes
+  @objc optional func segmentDidChange(control: MWSegmentedControl, value: Int)
+}
+
+class MWSegmentedControl: UIView {
+  var buttonTitles = ["7", "21", "30", "60", "90"]
+  var borderColor = UIColor(red:0.06, green:0.51, blue:1, alpha:1)
+  var textColor = UIColor.darkGray
+  var font = UIFont(name: "Avenir-Heavy", size: 36)
+  var delegate: MWSegmentedControlDelegate?
+  var selectedSegments = [String]()
+  var selectedIndexes = [2]
+  var allowMultipleSelection = false
+  var value: Int!
+
+  override func layoutSubviews() {
+    self.layer.cornerRadius = 10
+    self.layer.borderWidth = 2
+    self.layer.borderColor = self.borderColor.cgColor
+    self.layer.masksToBounds = true
+
+    if self.subviews.count <= 0 {
+      for (index, button) in buttonTitles.enumerated() {
+        let buttonWidth = self.frame.width / CGFloat(buttonTitles.count)
+        let buttonHeight = self.frame.height
+
+        let newButton = UIButton(frame: CGRect(
+          x: CGFloat(index) * buttonWidth,
+          y: 0,
+          width: buttonWidth,
+          height: buttonHeight
+        ))
+        newButton.setTitle(button, for: .normal)
+        newButton.setTitleColor(self.textColor, for: .normal)
+        newButton.titleLabel?.font = self.font!
+        newButton.addTarget(self, action: #selector(changeSegment), for: .touchUpInside)
+        newButton.layer.borderWidth = 1
+        newButton.layer.borderColor = self.borderColor.cgColor
+        newButton.tag = index
+        newButton.showsTouchWhenHighlighted = true
+        self.addSubview(newButton)
+
+        for selected in selectedIndexes {
+          if selected == index {
+            self.changeSegment(sender: newButton)
+          }
+        }
+      }
+    }
+  }
+
+  func changeSegment(sender: UIButton) {
+    if allowMultipleSelection {
+      if sender.backgroundColor == borderColor {
+        sender.backgroundColor = UIColor.clear
+        sender.setTitleColor(textColor, for: .normal)
+
+        for (index, segment) in self.selectedSegments.enumerated() {
+          if segment == sender.titleLabel!.text! {
+            self.selectedSegments.remove(at: index)
+            break
+          }
+        }
+
+      } else {
+        sender.backgroundColor = borderColor
+        sender.setTitleColor(UIColor.white, for: .normal)
+        self.value = NSString(string: sender.titleLabel!.text!).integerValue
+        self.selectedSegments.append(self.buttonTitles[sender.tag])
+        self.delegate?.segmentDidChange!(control: self, value: self.value)
+      }
+    } else {
+      for subview in self.subviews {
+        if subview is UIButton {
+          (subview as! UIButton).setTitleColor(self.textColor, for: .normal)
+          (subview as! UIButton).backgroundColor = UIColor.clear
+        }
+      }
+      sender.backgroundColor = borderColor
+      sender.setTitleColor(UIColor.white, for: .normal)
+      self.value = NSString(string: sender.titleLabel!.text!).integerValue
+      self.delegate?.segmentDidChange!(control: self, value: self.value)
+    }
+
+  }
+}
---
+++ EventNodeExample/EventNodeExample/Flows/Main/TreeNodeView.swift
@@ -0,0 +1,179 @@
+//
+//  TreeNodeView.swift
+//  EventNodeExample
+//
+//  Created by Rumata on 6/22/17.
+//  Copyright © 2017 DarthRumata. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Events
+
+@IBDesignable
+class TreeNodeView: UIView {
+
+  var eventNode: EventNode! {
+    didSet {
+      eventNode.addHandler { [weak self] (event: Event) in
+        guard let strongSelf = self else {
+          return
+        }
+
+        if case .nodeSelected(let view) = event {
+          let newState: SelectedState = view == strongSelf ? .selected : .normal
+          if strongSelf.selectedState == newState {
+            return
+          }
+
+          strongSelf.selectedState = newState
+        }
+      }
+
+      eventNode.addHandler { [weak self] (event: MainViewController.Event) in
+        guard let strongSelf = self else {
+          return
+        }
+
+        if event == .userSentEvent && strongSelf.selectedState == .selected {
+          strongSelf.eventNode.raise(event: Event.bubbleUserEvent(SharedTime()))
+        }
+      }
+
+      eventNode.addHandler(.onPropagate) { [weak self] (event: Event) in
+        guard let strongSelf = self else {
+          return
+        }
+
+        if case .bubbleUserEvent(let sharedTime) = event {
+          let scheduledTime = sharedTime.startTime
+          sharedTime.startTime = sharedTime.startTime + 1
+          DispatchQueue.main.asyncAfter(deadline: scheduledTime) {
+            strongSelf.highlightedState = .onPropagate
+            DispatchQueue.main.asyncAfter(deadline: scheduledTime + 1) {
+              strongSelf.highlightedState = .none
+            }
+          }
+        }
+      }
+
+      eventNode.addHandler(.onRaise) { [weak self] (event: Event) in
+        guard let strongSelf = self else {
+          return
+        }
+
+        if case .bubbleUserEvent(let sharedTime) = event {
+          let scheduledTime = sharedTime.startTime
+          sharedTime.startTime = sharedTime.startTime + 1
+          DispatchQueue.main.asyncAfter(deadline: scheduledTime) {
+            strongSelf.highlightedState = .onRaise
+            DispatchQueue.main.asyncAfter(deadline: scheduledTime + 1) {
+              strongSelf.highlightedState = .none
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @IBOutlet fileprivate weak var titleLabel: UILabel!
+  @IBOutlet fileprivate weak var eventDirectionMarker: UIImageView!
+
+  @IBInspectable
+  var title: String = "" {
+    didSet {
+      self.titleLabel.text = title
+    }
+  }
+
+  fileprivate var selectedState: SelectedState = .normal {
+    didSet {
+      updateBackground()
+    }
+  }
+
+  fileprivate var highlightedState: HighlightedState = .none {
+    didSet {
+      updateIcon()
+    }
+  }
+
+  // MARK: Init
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+
+    let nib = UINib(nibName: String(describing: TreeNodeView.self), bundle: nil)
+    let content = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+    addSubview(content)
+    content.frame = bounds
+  }
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+
+    layer.cornerRadius = 5
+    layer.masksToBounds = true
+  }
+
+  // MARK: Actions
+
+  @IBAction func didTapOnView() {
+    let event = Event.nodeSelected(self)
+    eventNode.raise(event: event)
+  }
+
+}
+
+extension TreeNodeView {
+
+  enum Event: Events.Event {
+    case nodeSelected(TreeNodeView?)
+    case bubbleUserEvent(SharedTime)
+  }
+
+  class SharedTime {
+    var startTime: DispatchTime = .now()
+  }
+
+}
+
+private extension TreeNodeView {
+
+  /// TODO: Can be optimized by merging to states in OptionSet
+  enum SelectedState {
+    case normal, selected
+
+    var color: UIColor {
+      switch self {
+      case .normal:
+        return .lightGray
+
+      case .selected:
+        return .green
+
+      }
+    }
+  }
+
+  enum HighlightedState {
+    case onPropagate, onRaise, none
+
+    var icon: UIImage? {
+      switch self {
+      case .onPropagate: return UIImage(named: "arrowDown")
+      case .onRaise: return UIImage(named: "arrowUp")
+      case .none: return nil
+      }
+    }
+  }
+  
+  func updateBackground() {
+    backgroundColor = selectedState.color
+  }
+
+  func updateIcon() {
+    eventDirectionMarker.image = highlightedState.icon
+  }
+  
+}
